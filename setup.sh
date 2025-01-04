f872695e2461aff0eceab593029a21644898d314
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-04 20:49:58 +0100

    Setup zsh configuration sync service.

--- setup.sh
+++ setup.sh
@@ -48,4 +48,40 @@ echo "Please log out and log back in for changes to take effect."
 echo "Installing Oh My Zsh..."
 su - "$ACTUAL_USER" -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
 
+########### Setup ZSH Config Sync Service ##########
+echo "Setting up ZSH config sync service..."
 
+# Make the script executable
+chmod +x /home/$ACTUAL_USER/.development-environment/zsh/sync.sh
+
+# Create systemd user directory
+mkdir -p /home/$ACTUAL_USER/.config/systemd/user/
+
+# Create the service file
+cat << EOF > /home/$ACTUAL_USER/.config/systemd/user/zshrc-sync.service
+[Unit]
+Description=ZSH Config Auto-Sync Service
+After=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /home/$ACTUAL_USER/.setup-development-environment/zshrc-sync.sh
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Set correct ownership
+chown -R $ACTUAL_USER:$ACTUAL_USER /home/$ACTUAL_USER/.config/systemd
+
+# Enable and start the service for the user
+su - $ACTUAL_USER -c 'systemctl --user enable zshrc-sync.service'
+su - $ACTUAL_USER -c 'systemctl --user start zshrc-sync.service'
+
+# Enable user services to run on boot
+loginctl enable-linger $ACTUAL_USER
+
+echo "ZSH config sync service has been set up and started."
+echo "You can check the service status with: systemctl --user status zshrc-sync.service"

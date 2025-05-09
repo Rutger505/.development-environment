5f5095d47e031121ef82d6e46401433c709c07bb
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-10 01:11:50 +0200

    Rename to neovim and insatll appimage safely

---
+++ applications/cli/neovim.sh
@@ -0,0 +1,7 @@
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
+git clone https://github.com/NvChad/starter ~/.config/nvim
+

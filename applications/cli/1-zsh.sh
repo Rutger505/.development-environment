fd2aa28d84d05702c0d027fff729b73a082f0ab4
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-10 00:06:58 +0200

    Merge zsh and oh my zsh

--- applications/cli/1-zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install zsh
-
-echo "Changing default shell to Zsh. Please enter password below"
-chsh -s "$(which zsh)"
--- applications/cli/zsh-with-omz.sh
+++ applications/cli/zsh-with-omz.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+sudo apt-get install zsh
+
+echo "Changing default shell to Zsh. Please enter password below"
+chsh -s "$(which zsh)"
+
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "Removing $HOME/.zshrc file to prevent GNU Stow conflicts"
 rm -f "$HOME/.zshrc"
+

b86fab21caf2b2a654629c9486f15f010e93f188
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-12 09:38:56 +0200

    Run tpm update when tmux config changes

--- synchronization/update-dotfiles.sh
+++ synchronization/update-dotfiles.sh
@@ -1,6 +1,15 @@
-(
-  cd ~/.development-environment && \
-  git pull && \
-  stow .
-) 2>&1 | tee ~/.development-environment/synchronization/update-dotfiles.log
+# Redirect all output (stdout and stderr) to a log file
+exec >> ~/.development-environment/synchronization/update-dotfiles.log 2>&1
+
+# Your script content below
+echo "Script started at $(date)"
+
+cd ~/.development-environment
+git pull
+stow .
+
+if git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD | grep -q '^\.tmux\.conf$'; then
+  echo ".tmux.conf was modified! Running tpm update"
+  ~/.tmux/plugins/tpm/bin/update_plugins all
+fi
 

036e5d0e464cb077fa8dd9e5258df90e97d8f685
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-20 23:46:03 +0200

    Install gcc for neovim and don't clone nvchad starter

--- applications/cli/neovim.sh
+++ applications/cli/neovim.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install gcc
+
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
 
 sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
 
-git clone https://github.com/NvChad/starter ~/.config/nvim
 

ad8e45e9412ee3c1a28982743de4f3cc199c7d16
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-20 23:40:10 +0200

    Fix toolbox auto starting when installing. Closes #10

--- applications/desktop/jetbrains-toolbox.sh
+++ applications/desktop/jetbrains-toolbox.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+# https://github.com/Rutger505/.development-environment/issues/10
+export CI="skip-startup" && curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash

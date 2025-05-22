2da1f2436cd7eaee00e327967ae7b35ed6fee309
Rutger Pronk <rutger.pronk11@gmail.com>
2025-05-22 16:40:25 +0200

    Add zen autostart

--- applications/desktop/zen.sh
+++ applications/desktop/zen.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo flatpak install flathub app.zen_browser.zen
+
+echo "Configuring autostart for Zen"
+sudo ln /var/lib/flatpak/exports/share/applications/app.zen_browser.zen.desktop ~/.config/autostart/zen-browser.desktop

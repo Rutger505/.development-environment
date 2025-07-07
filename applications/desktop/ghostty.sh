2003f487a4bf7240f2927aa6020405aae19ac165
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-07 11:52:35 +0200

    Add vi keybindings, add ghostty autostart configuration

--- applications/desktop/ghostty.sh
+++ applications/desktop/ghostty.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo snap install ghostty --classic
+
+echo "Configuring autostart configuration"
+cat <<EOF > ~/.config/autostart/ghostty.desktop
+[Desktop Entry]
+Name=Ghostty
+Exec=ghostty
+Type=Application
+EOF

f9cfd7aec861430a740257bef18cd39c584af308
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-26 22:04:13 +0200

    Add proper tmux install and update plugins

--- applications/cli/tmux.sh
+++ applications/cli/tmux.sh
@@ -4,3 +4,4 @@ sudo yay -S tmux
 
 git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
 
+~/.local/share/tmux/plugins/tpm/bin/install_plugins all
--- synchronization/update-dotfiles.sh
+++ synchronization/update-dotfiles.sh
@@ -5,16 +5,12 @@ exec >> ~/.development-environment/synchronization/update-dotfiles.log 2>&1
 echo "Script started at $(date)"
 
 cd ~/.development-environment || exit 1
+
 git pull
+
 stow .
 
 if git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD | grep -q '^\.tmux\.conf$'; then
   echo ".tmux.conf was modified! Running tpm update"
-  ~/.tmux/plugins/tpm/bin/update_plugins all
+  ~/.local/share/tmux/plugins/tpm/bin/update_plugins all
 fi
-
-
-# Was changed to `init-prettier.sh`
-if [ -f "$HOME/init_prettier.sh" ]; then
-	rm "$HOME/init_prettier.sh"
-fi;

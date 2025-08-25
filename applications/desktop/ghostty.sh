f5aa6ac4a0e8a2896cef8e6bf7df0edd26ca9491
Rutger Pronk <rutger.pronk11@gmail.com>
2025-08-25 17:28:37 +0200

    Try and install gostty from official repository

--- applications/desktop/ghostty.sh
+++ applications/desktop/ghostty.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sudo snap install ghostty --classic
-
-echo "Configuring autostart configuration"
-cat <<EOF > ~/.config/autostart/ghostty.desktop
-[Desktop Entry]
-Name=Ghostty
-Exec=ghostty
-Type=Application
-EOF
+yay -S ghostty

aaa7dbc53e9cf650d05f7b0bdc63a3f8b5e10d74
Rutger Pronk <rutger.pronk11@gmail.com>
2025-08-26 16:09:43 +0200

    Install jetbrains toolbox with aur

--- applications/desktop/jetbrains-toolbox.sh
+++ applications/desktop/jetbrains-toolbox.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# https://github.com/Rutger505/.development-environment/issues/10
-export CI="skip-startup" && curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+yay -S jetbrains-toolbox

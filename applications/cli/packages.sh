a9c3925ececbfdd3fe7a3f274539efe02f71693b
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-05 10:19:24 +0200

    Install discord from arch repositories and install btop in packages

--- applications/cli/packages.sh
+++ applications/cli/packages.sh
@@ -5,4 +5,5 @@ yay -S \
 	eza \
 	neovim \
 	k9s \
-  cloc
+  cloc \
+  btop
--- applications/desktop/discord.sh
+++ applications/desktop/discord.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-flatpak install flathub com.discordapp.Discord
 
-
-echo "Configuring autostart for Discord"
-ln -s ~/.local/share/flatpak/exports/share/applications/com.discordapp.Discord.desktop ~/.config/autostart/bitwarden.desktop
+flatpak remove webcord
+yay -S discord

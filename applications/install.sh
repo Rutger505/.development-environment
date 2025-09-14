90280dff9944418658b4592d5eb3469817977cd3
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-14 16:53:29 +0200

    Extract package list to seperate plain text files and always install them

--- applications/install.sh
+++ applications/install.sh
@@ -1,120 +1,30 @@
 #!/bin/bash
 
-SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIRECTORY="$(dirname "$SCRIPT_PATH")"
 cd "$SCRIPT_DIRECTORY" || exit 1
 
 source "$SCRIPT_DIRECTORY/functions.sh"
 
-PACKAGES=(
-  # System essentials
-  "os-prober"
-  "sddm" # TODO enable autostart
-  "hyprland" 
-  "dunst" # TODO enable autostart
-  "pipewire" # TODO enable autostart
-  "wireplumber"
-  "hyprpolkitagent"
-  "xdg-desktop-portal-hyprland"
-  "xdg-desktop-portal-gtk"
-  "nwg-displays"
-  "qt5-wayland"
-  "qt6-wayland"
-  "pavucontrol"
-  # TODO maybe add and configure qt(5/6)ct
-  "hyprqt6engine"
-  "hyprcursor"
-  "hyprland-qt-support"
-  "ttf-jetbrains-mono-nerd"
-  "nwg-look"
-  "waybar"
-  "hyprpaper"
-  "hypridle"
-  "hyprlock"
-  "hyprpicker"
-  "wofi"
-  "cliphist"
-  "dolphin"
-
-  # Lol
-  sl
-
-  # User Essential
-  "stow"
-  "git"
-  "curl"
-  "wget"
-  "unzip"
-  "zip"
-  "jq"
-  "cloc"
-  "zsh"
-  "snapd"
-  "cronie"
-  "magic-wormhole"
-  "gparted"
-
-  # Applications used in dotfiles
-  "zoxide"
-  "starship"
-  "neovim"
-  "kitty"
-  "ghostty"
-  # Basic enhancements
-  "exa"
-  "bat"
-  "ripgrep"
-  "fd"
-  "fzf"
-  "btop"
-)
 SERVICE_PACKAGES=(
-  "snapd" "cronie" "sddm"
+  "snapd" "cronie" "sddm" "kanata"
 )
 AUTOSTART_PACKAGES=()
 
-prompt_for_confirmation "kanata" "true" "service"
-# JS/TS
-prompt_for_confirmation "nvm"
-prompt_for_confirmation "bun"
-# Github
-prompt_for_confirmation "github-cli"
-# DevOps
-prompt_for_confirmation "opentofu"
-prompt_for_confirmation "kubectl"
-prompt_for_confirmation "k9s"
-# Power
-prompt_for_confirmation "powertop"
-prompt_for_confirmation "powerstat"
-# GPU / IGPU
-prompt_for_confirmation "envycontrol"
-# Shell
-prompt_for_confirmation "tmux"
-# Password Manager
-prompt_for_confirmation "bitwarden"
-# Browsers
-prompt_for_confirmation "chromium"
-prompt_for_confirmation "firefox"
-prompt_for_confirmation "zen-browser-bin"
-
-# Communication
-prompt_for_confirmation "discord"
-prompt_for_confirmation "slack-desktop"
-# Editors
-prompt_for_confirmation "jetbrains-toolbox"
-prompt_for_confirmation "visual-studio-code-bin"
-# Gaming
-prompt_for_confirmation "steam"
-prompt_for_confirmation "minecraft-launcher"
-prompt_for_confirmation "prismlauncher"
-
-# Install paru
 if ! command -v paru >/dev/null 2>&1; then
   echo "Paru not found, installing..."
 
   install_paru
 fi
 
-paru -Sy --needed "${PACKAGES[@]}"
+find "$SCRIPT_DIRECTORY/package-lists/" -name "*.lst" -print0 | \
+  xargs -0 cat | \
+  grep -vE '^\s*#' | \
+  grep -vE '^\s*$' | \
+  paru -Sy --needed -
+
+
+
 
 run_post_install_scripts "$SCRIPT_DIRECTORY" "${PACKAGES[@]}"
 enable_services "${SERVICE_PACKAGES[@]}"

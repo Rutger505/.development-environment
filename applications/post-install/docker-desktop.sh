3c0b9f2f33434a61cf57fee1bd0fe5b2810a1ddf
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-17 12:41:31 +0200

    Add docker desktop

---
+++ applications/post-install/docker-desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker context create desktop-linux \
+  --description "Docker Desktop Linux" \
+  --docker "host=unix://$HOME/.docker/desktop/docker.sock"
+docker context use desktop-linux

5d0f82bca857c7f9bc5a13cbf097a69abb61b5d0
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-17 16:58:24 +0200

    MAke grub 2 seconds, add 1080p res

--- applications/post-install/grub.sh
+++ applications/post-install/grub.sh
@@ -3,8 +3,8 @@
 echo "Enabeling os-prober"
 sudo sed -i -E 's/^#?GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 
-echo "Shorting timeout to 1 second"
-sudo sed -i -E 's/^#?\s*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=1/' /etc/default/grub
+echo "Shorting timeout to 2 second"
+sudo sed -i -E 's/^#?\s*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/' /etc/default/grub
 
 sudo grub-install --target=x86_64-efi \
 	--bootloader-id=GRUB \

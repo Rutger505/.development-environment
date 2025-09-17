06a4f91f821d5a5640efe0a34ed050ccbc144d96
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-17 13:11:32 +0200

    Add docker group and desktop 's bin export

--- .config/zsh/packages/docker.zsh
+++ .config/zsh/packages/docker.zsh
@@ -1,1 +1,2 @@
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+export PATH="/opt/docker-desktop/bin:$PATH"
--- applications/post-install/docker-desktop.sh
+++ applications/post-install/docker-desktop.sh
@@ -4,3 +4,5 @@ docker context create desktop-linux \
   --description "Docker Desktop Linux" \
   --docker "host=unix://$HOME/.docker/desktop/docker.sock"
 docker context use desktop-linux
+
+sudo usermod -aG docker "$USER"

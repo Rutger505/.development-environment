43bdad4ca2f1ffde1807d405afa9a318ce779205
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-19 13:15:14 +0200

    Add autostart services, start adding chaotic aur, make disable wpa excecutable

---
+++ applications/post-install/chaotic-aur.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+
+# TODO: only run this when a chaotic aur entry doesn't eist yet
+echo "Adding chaotic aur to pacman.conf"
+cat <<EOF >> sudo tee /etc/pacman.conf
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
+
+

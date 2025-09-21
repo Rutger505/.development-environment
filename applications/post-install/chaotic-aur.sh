3c9d87b96983e6eaf941c9a7e34bd6d9fd9ddd5a
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-21 13:48:30 +0200

    Finish chaotic aur script

--- applications/post-install/chaotic-aur.sh
+++ applications/post-install/chaotic-aur.sh
@@ -3,16 +3,20 @@
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 
-# TODO: only run this when a chaotic aur entry doesn't eist yet
-echo "Adding chaotic aur to pacman.conf"
-cat <<EOF >> sudo tee /etc/pacman.conf
+# Only add chaotic-aur entry if it doesn't exist yet
+if ! grep -q '^\[chaotic-aur\]' /etc/pacman.conf; then
+	echo "Adding chaotic aur to pacman.conf"
+	sudo tee -a /etc/pacman.conf > /dev/null <<EOF
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 EOF
+else
+	echo "chaotic-aur entry already exists in pacman.conf, skipping."
+fi
 
 
 

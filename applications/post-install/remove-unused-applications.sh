bb0bb10b0804c84a25f7e39f3b580edd32a2d339
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-25 14:48:39 +0100

    Remove discord and other applciations unused and are annoying

--- applications/post-install/remove-unused-applications.sh
+++ applications/post-install/remove-unused-applications.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env zsh
 
 paru -R signal-desktop 1password-beta
+
+cd ~/.local/share/applications || (echo "Can't change directory to ~/.local/share/applications" && exit 1)
+rm Basecamp.desktop
+rm Discord.desktop # The omarchy-web-application variant    
+rm HEY.desktop

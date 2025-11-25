bba29c547278bab3fcc87fb190307784ac0fd7ab
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-25 14:02:26 +0100

    Try and find tmux session based on stringified tag

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -1,5 +1,5 @@
 if command -v tmux > /dev/null 2>&1; then
   if [[ -z "$TMUX" ]] && [[ $- == *i* ]] && [[ "$TERM" != "tmux-256color" && "$TERM" != "xterm-256color" ]]; then
-    tmux attach-session -t 0 2>/dev/null || tmux new-session -s 0
+    tmux attach-session -t "0" 2>/dev/null || tmux new-session -s "0"
   fi
 fi

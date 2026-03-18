1eca5d8734ee1bd8fbdc7fcd8a926f6e8b19eb54
Rutger Pronk <rutger.pronk11@gmail.com>
2026-03-18 17:05:55 +0100

    Use defaults for gitsigns

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -18,15 +18,7 @@ return {
 
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
+		opts = {},
 	},
 
 	{

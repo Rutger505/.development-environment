f26ba97d6ad6220805ed465c0d3b71d36a44a18c
Rutger Pronk <rutger.pronk11@gmail.com>
2026-03-19 17:07:42 +0100

    Add no modify path flag

--- applications/packages/optional/opencode.sh
+++ applications/packages/optional/opencode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -fsSL https://opencode.ai/install | bash && mv ~/.opencode ~/.local/share/opencode
+curl -fsSL https://opencode.ai/install | bash -s -- --no-modify-path && mv ~/.opencode ~/.local/share/opencode
